965647698303f173a53f7b90c63dcd9481b96b5e
Micah Walter <micah@micahwalter.com>
2014-06-06 17:21:48 -0400

    adding mongoose

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 my-little-node-app
 ===
+
+a starter template for a node project
+
+uses
+---
+
+* express for routes n stuff
+* swig for templates
+* mongoose for talking to mongo
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var swig  = require('swig');
+var mongoose = require('mongoose');
 
 var app = express();
 
@@ -11,6 +12,11 @@ app.use(express.static(__dirname + '/app/public'));
 if (!process.env.NODE_ENV){
 	swig.setDefaults({ cache: false });
 }
+
+var configDB = require(__dirname + '/config/database.js');
+mongoose.connect(configDB.url);
+
+
 
 require('./app/routes/routes')(app);
 
