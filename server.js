b6978c7d0f279f1061caa49ee3e841e51c525940
Micah Walter <micah@micahwalter.com>
2014-06-06 17:07:16 -0400

    check for NODE_ENV

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/app/templates');
 app.use(express.static(__dirname + '/app/public'));
 
+if (!process.env.NODE_ENV){
+	swig.setDefaults({ cache: false });
+}
+
 require('./app/routes/routes')(app);
 
 var port = Number(process.env.PORT || 5000);
