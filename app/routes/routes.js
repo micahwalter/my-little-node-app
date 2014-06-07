06ace69e7c850d6912f32ce1aa599409ef891bdb
Micah Walter <micah@micahwalter.com>
2014-06-07 15:44:11 -0400

    switched to express 4 way of routing

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,18 +1,22 @@
 // you can put all your routes in here if you like.
+var express = require('express');
 
 module.exports = function(app){
 	
+	var router = express.Router();
+	
 	// Home route
     var index = require('../index');
-    app.get('/', index.render);
+    router.get('/', index.render);
 	
 	// Post route
     var post = require('../post');
-    app.get('/post/:id', post.render);
+    router.get('/post/:id', post.render);
 
 	// Creates a new post
     var newPost = require('../newPost');
-    app.get('/new', newPost.render);
+    router.get('/new', newPost.render);
 	
+	app.use('/', router);
 	
 };
